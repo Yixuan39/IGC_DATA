65be66ec99759a1c7cc310de85c648ff24bbf5d9
Ethan Yang <yyang55@ncsu.edu>
2020-12-30 20:51:17 +0800

    add some new data

--- IGCtableMaker.R
+++ IGCtableMaker.R
@@ -1,24 +1,28 @@
 
-
-FileName = "pillar211.pdf"
-
-
-
-file = paste(c("/Users/yixuanyang/Desktop/IGC tables/", FileName), collapse = "")
+name = "521original"
+FileName = paste(c(name,".pdf"))
+file = paste(c("/Users/yixuanyang/Desktop/IGC_DATA", FileName), collapse = "")
 library(gridExtra)
+library(gtable, grid)
 library(datasets)
+library(dplyr)
 setwd("/Users/yixuanyang/Desktop")
 
-Value <- readLines(paste( "/Users/yixuanyang/Desktop/IGC_DATA/MG94_01_02_nonclockqq_summary.txt", sep = ""),n = 89)
-Value <- as.numeric(Value)
-Name <- readLines(paste( "/Users/yixuanyang/Desktop/IGC_DATA/MG94_01_02_nonclockqq_summary.txt", sep = ""))
+#Value <- readLines(paste( "/Users/yixuanyang/Desktop/IGC_DATA/MG94_01_02_nonclockqq_summary.txt", sep = ""),n = 89)
+#Value <- as.numeric(Value)
+#Name <- readLines(paste( "/Users/yixuanyang/Desktop/IGC_DATA/MG94_01_02_nonclockqq_summary.txt", sep = ""))
 
-Name <- unlist(strsplit(Name[90], split=" "))
-Name <- Name[!(Name %in% Name[1])]
-table <- data.frame(Name, Value)
-
-pdf(file, height = 25)
-grid.table(table)
+#Name <- unlist(strsplit(Name[90], split=" "))
+#Name <- Name[!(Name %in% Name[1])]
+#table <- data.frame(Name, Value)
+table <- MGc4_i1[,7] %>% as.matrix() %>% round(digits = 4)
+table <- data.frame(rownames(table), table)
+colnames(table) <- c(name, 'values')
+pdf(file, height = 28)
+grid.table(table, rows = c(1:89))
 dev.off()
 
+
+#testForSwap  <- readLines("/Users/yixuanyang/Downloads/MG94_01_02_nonclockqq_summary.txt",  n = 89) %>% as.numeric() %>% as.matrix(byrow=TRUE, ncol= 1) 
+#rownames(testForSwap) <- NameList
 
---
+++ iniC3/4287/Run_IS_IGC_Old.py
@@ -0,0 +1,32 @@
+#! /usr/bin/python3
+
+from IGCexpansion.CodonGeneconv import ReCodonGeneconv
+import argparse, os
+import numpy as np
+
+def check_folder(folder_name):
+    # if the folder doesn't exist, 
+    # this function creats it by assuming the outer folder exists (potential bug...)
+    if not os.path.isdir(folder_name):
+        os.mkdir(folder_name)
+
+if __name__ == '__main__':
+    paralog = ['01', '02']
+    Force = None
+    alignment_file = './Pillar4287Swapped.fasta'
+    newicktree = './Pillar4287Swapped.newick'
+    Force = None
+    model = 'MG94'  # choose from 'HKY' and 'MG94'
+    save_folder = './save/'
+    check_folder(save_folder)
+    save_name = save_folder + model + '4287iniC3.txt'
+
+    summary_folder = './summary/'
+    check_folder(summary_folder)
+
+    test = ReCodonGeneconv( newicktree, alignment_file, paralog, Model = model, Force = Force, clock = None, save_path = './save/', save_name = save_name,
+                            tau=3.5, omega=1.0, kappa=1.0, inibl=0.1)
+    test.get_mle()
+    test.get_ExpectedNumGeneconv()
+
+    test.get_individual_summary(summary_path='./save/')
